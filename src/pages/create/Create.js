349ad4c6bc0b08ca67df9527efcf8e528db1e982
Sumanth U <sumanth2ky9@gmail.com>
2025-08-10 22:05:20 +0530

    chore: add list of users

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,7 +1,8 @@
 // styles
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Select from 'react-select'
 import './Create.css'
+import { useFetchUsers } from '../../hooks/useFetchUsers'
 
 const categories = [
   {value: 'development', label: 'Development'},
@@ -18,12 +19,18 @@ export default function Create() {
   const [category, setCategory] = useState('')
   const [assignedUsers, setAssignedUsers] = useState([])
   const [formError, setFormError] = useState(null)
+  const {userList, fetchError, now, isPending} = useFetchUsers()
+  
 
   const handleSubmit = async (e) =>{
     e.preventDefault()
 
     console.log(name, details, dueDate, category.value, assignedUsers)
   }
+
+  useEffect(() => {
+    console.log("Printing the users list: ", userList)
+  }, [])
 
   return (
     <div className='create-form'>
@@ -66,8 +73,11 @@ export default function Create() {
         <label>
           <span>Assign to:</span>
           <Select
+            // isOptionDisabled={isPending}
             onChange={option => setAssignedUsers(option)}
-            options={}
+            // isLoading={isPending}
+            // isSearchable={!isPending}
+            options={userList ? userList.map(user => ({value: user.id, label: `${user.user_first_name} ${user.user_last_name}`})) : 'loading'}
           />
         </label>
 
